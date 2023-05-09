8654d8790028cfc0c374427de6b3562f2634fc9e
Hamilton Durán <hamy.duran@gmail.com>
2023-05-08 22:29:16 -0600

    reeestructurando el codigo JS en modulos

--- service/client-service.js
+++ service/client-service.js
@@ -1,37 +1,4 @@
-const crearNuevaLinea =  (nombre,email) =>{
-    const linea = document.createElement("tr");
-    const contenido = `
-            <td class="td" data-td>${nombre}</td>
-            <td>${email}</td>
-            <td>
-              <ul class="table__button-control">
-                <li>
-                  <a
-                    href="../screens/editar_cliente.html"
-                    class="simple-button simple-button--edit"
-                    >Editar</a
-                  >
-                </li>
-                <li>
-                  <button
-                    class="simple-button simple-button--delete"
-                    type="button"
-                  >
-                    Eliminar
-                  </button>
-                </li>
-              </ul>
-            </td>`;
-            linea.innerHTML = contenido;
-            return linea;
-};
-
-const table = document.querySelector("[data-table]")
-
-
-
 //Abrir HTTP (metodo,url)
-
 //CRUD - metodos HTTP
 //Create - POST
 //Read - GET
@@ -41,12 +8,13 @@ const table = document.querySelector("[data-table]")
 
 //FETCH API
 const listaClientes = () => fetch("http://localhost:3000/perfil").then((respuesta) => respuesta.json());
-listaClientes().then((data) =>{
-    data.forEach((perfil) =>{
-    const nuevaLinea = crearNuevaLinea(perfil.nombre,perfil.email)
-        table.appendChild(nuevaLinea);
-    });
-}).catch((error) => alert("Ocurrio un error"));
+
+//SE ENCARGA DE HACER LA COMUNICACIO CO EL SERVIDOR Y RECIBIR LA RESPUESTA
+
+export const clientServices = {
+  listaClientes,
+};
+
 
 
 
