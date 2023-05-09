cd7401f5818dc908ca295014834625b9f9c46a0f
Hamilton Durán <hamy.duran@gmail.com>
2023-05-08 21:25:04 -0600

    Configuración base y se reescribio el codigo para usar FETCH API

--- service/client-service.js
+++ service/client-service.js
@@ -38,24 +38,9 @@ const table = document.querySelector("[data-table]")
 //Update - PUT/PATCH
 //Delete - DELETE
 
-const listaClientes = () =>{
-    const promise = new Promise(function(resolve,reject){
-    const http =  new XMLHttpRequest();
-    http.open("GET","http://localhost:3000/perfil");
 
-    http.send();
-
-    http.onload = () => {
-    const response = JSON.parse(http.response);
-    if(http.status>= 400){
-        reject(response);
-    }else{
-        resolve(response);
-    }
-    };
-    });
-    return promise;
-};
+//FETCH API
+const listaClientes = () => fetch("http://localhost:3000/perfil").then((respuesta) => respuesta.json());
 listaClientes().then((data) =>{
     data.forEach((perfil) =>{
     const nuevaLinea = crearNuevaLinea(perfil.nombre,perfil.email)
