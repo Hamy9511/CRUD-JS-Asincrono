f2134226fdaa3f53d562ab8079fc80beb71bb758
Hamilton Durán <hamy.duran@gmail.com>
2023-05-13 20:43:23 -0600

    Asinc/Await

--- controllers/actualizar-controller.js
+++ controllers/actualizar-controller.js
@@ -2,7 +2,7 @@ import { clientServices } from "../service/client-service.js";
 
 const formulario = document.querySelector("[data-form]");
 
-const obtenerInformacion = () => {
+const obtenerInformacion = async () => {
     const url = new URL(window.location);
     const id = (url.searchParams.get("id"));
 
@@ -12,23 +12,20 @@ const obtenerInformacion = () => {
 
 const nombre = document.querySelector("[data-nombre]")
 const email = document.querySelector("[data-email]")
-
-    clientServices.detalleCliente(id).then((perfil) => {
+const perfil = await clientServices.detalleCliente(id);
         nombre.value = perfil.nombre;
         email.value = perfil.email;
-    });
 };
 
 obtenerInformacion();
 
-formulario.addEventListener("submit",(evento) =>{
+formulario.addEventListener("submit",async (evento) =>{
     evento.preventDefault() //evitar que el formulario trate de hacer la peticion y usamos js
     const url = new URL(window.location);
     const id = (url.searchParams.get("id"));
 
     const nombre = document.querySelector("[data-nombre]").value;
     const email = document.querySelector("[data-email]").value;
-    clientServices.actualizarCliente(nombre,email,id).then(()=>{
+    const actualizar = await clientServices.actualizarCliente(nombre,email,id);
         window.location.href = "/screens/edicion_concluida.html";
-    })
 });
